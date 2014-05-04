4e871e4a8e180e57c3ca522dbded3fe7d925e0c4
Carsten Zimmermann <cz@aegisnet.de>
2014-05-04 16:54:22 +0200

    EconomicSector has_many suspects

--- app/models/economic_sector.rb
+++ app/models/economic_sector.rb
@@ -1,2 +1,3 @@
 class EconomicSector < ActiveRecord::Base
+  has_many :suspects
 end
--- app/models/suspect.rb
+++ app/models/suspect.rb
@@ -1,3 +1,5 @@
 class Suspect < ActiveRecord::Base
+  belongs_to :economic_sector
+
   validates :name, :url, presence: true
 end
